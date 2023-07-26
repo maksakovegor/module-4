1eb8abfce655962ae59960cef3bbf353e6e3cdb3
EGOR <egormaksakov2008@gmail.com>
2023-07-26 16:01:51 +0300

    дз

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpRespounse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpRespounse('True')
+    return render(request, 'index.html')
