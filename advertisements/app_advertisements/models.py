6fefbaa9d513b03d64cc4a569b73b09f24f6000c
EGOR <egormaksakov2008@gmail.com>
2023-08-14 21:34:41 +0300

    дз

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
     description = models.TextField("описание")
     price = models.DecimalField("цена", max_digits=10, decimal_places=2)
-    auktion = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
